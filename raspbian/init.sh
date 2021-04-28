7fed1d2de92020fb1067c707259c910e5b9c3c96
TheMegaNear <fluke34261@users.noreply.github.com>
2021-04-28 20:59:18 +0700

    Update init.sh

--- raspbian/init.sh
+++ raspbian/init.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+rm ./wpa_supplicant.conf
+rm ./SSH
+
 curl -OL https://raw.githubusercontent.com/fluke34261/rpi-setup/main/raspbian/wpa_supplicant.conf
 sed -i '' "s/your-networks-SSID/$1/g" ./wpa_supplicant.conf
 sed -i '' "s/your-networks-password/$2/g" ./wpa_supplicant.conf
