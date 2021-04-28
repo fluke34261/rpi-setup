e9e777330a62d96a289e5410d3376ee87c77c5cc
TheMegaNear <fluke34261@users.noreply.github.com>
2021-04-28 11:02:16 +0700

    Update set_static_ip_without_monitor.sh

--- raspbian/set_static_ip_without_monitor.sh
+++ raspbian/set_static_ip_without_monitor.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh pi@$1 'curl https://raw.githubusercontent.com/fluke34261/rpi-setup/main/raspbian/set_static_ip.sh | sh -s $2'
+ssh pi@$1 "curl https://raw.githubusercontent.com/fluke34261/rpi-setup/main/raspbian/set_static_ip.sh | sh -s $2"
