bd07d3ac26b8ce19362d860ffaa31670b5cf1481
TheMegaNear <fluke34261@users.noreply.github.com>
2021-05-06 13:01:34 +0700

    Update set_wifi_and_ip.sh

--- ubuntu/set_wifi_and_ip.sh
+++ ubuntu/set_wifi_and_ip.sh
@@ -2,9 +2,14 @@ cat <<EOT >> ./network-config
 
 wifis:
  wlan0:
-   dhcp4: on
+   dhcp4: no
    # optional: true
+   addresses: [192.168.1.$3/24]
+   gateway4: 192.168.1.1
+   nameservers:
+     addresses: [8.8.8.8, 8.8.4.4]
    access-points:
      "$1":
        password: "$2"
+       
 EOT
